0869f360378b271545a9b70fcb5b482dc6e1bdef
Deaath65 <deaath65@gmail.com>
2023-06-30 10:19:32 +0545

    register/unregister users in admin

--- social/musker/admin.py
+++ social/musker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+
+# Unregistering gropus and user in Admin section
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    
+
+admin.site.register(User, UserAdmin)
