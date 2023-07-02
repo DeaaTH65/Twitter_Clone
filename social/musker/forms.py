5f1259e2fecbfe066dcc22e5df50c882478fe2a8
Deaath65 <deaath65@gmail.com>
2023-07-02 16:35:42 +0545

    meep post added in website

---
+++ social/musker/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Meep
+
+
+
+class MeepForm(forms.ModelForm):
+    body = forms.CharField(required=True, widget=forms.widgets.Textarea(attrs={"placeholder": "Enter your meeps", "class": "form-control-"}), label="")
+    
+    class Meta:
+        model = Meep
+        exclude = ("user",)
