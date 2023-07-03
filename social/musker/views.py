fd712b40dad28a392d0de1e3025fd3638ebbd023
Deaath65 <deaath65@gmail.com>
2023-07-03 11:29:03 +0545

    register user

--- social/musker/views.py
+++ social/musker/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Profile, Meep
 from django.contrib import messages
-from .forms import MeepForm
+from .forms import MeepForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 
@@ -53,7 +55,7 @@ def profile(request, pk):
     
     
 def login_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
@@ -72,3 +74,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out.!"))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()  
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #first_name = form.cleaned_data['first_name']
+            #last_name = form.cleaned_data['last_name']
+            #email = form.cleaned_data['email']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have been registered successfully!"))
+            return redirect('home')
+    return render(request, 'register.html', {'form': form})
