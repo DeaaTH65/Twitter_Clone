a9947de81645b257714984a88bc74636d065386b
Deaath65 <deaath65@gmail.com>
2023-07-03 16:36:43 +0545

    picture added/media

--- social/musker/models.py
+++ social/musker/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
     date_modified = models.DateTimeField(User, auto_now=True)
+    profile_img = models.ImageField(null=True, blank=True, upload_to='image/')
     
     def __str__(self):
         return self.user.username
